f35190c9d478568084ae65173597e7b6d2f8b3ad
XD133713 <xd4133713@gmail.com>
2026-03-12 16:51:51 +0100

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -6,3 +6,10 @@ class SavedCalculatorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'user', 'created_at')
     list_filter = ('user',)
     search_fields = ('name',)
+
+@admin.register(SavedCalculator)
+class UserCounterAdmin(admin.ModelAdmin):
+    list_display = ('user', 'login_amount', 'calculator')
+    search_fields = ('user_email',)
+
+
