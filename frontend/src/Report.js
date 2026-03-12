5f40417d2e6fdcac63cee923d6546da5d3a1a434
XD133713 <xd4133713@gmail.com>
2026-03-12 06:56:11 +0100

    Update Report.js

--- frontend/src/Report.js
+++ frontend/src/Report.js
@@ -1,1 +1,28 @@
 import React, { useEffect, useState } from "react";
+import API_URL from "./api";
+
+function Report() {
+  const [report, setReport] = useState(null);
+  
+  const fetchData = async () => {
+      const token = localStorage.getItem("access");
+      const res = await fetch(`${API_URL}/accounts/report/`, {headers: {Authorization: `Bearer ${token}`}})
+      const data = await res.json();
+      setReport(data);
+    }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!report) return <p>Loading...</p>;
+  
+  return (
+      <div>
+        <h1>Raport</h1>
+        <p>Liczba logowań: {report.login_amount}</p>
+        <p>Liczba utworzonych kalkulatorów: {report.calculator_amount}</p>
+      </div>
+    )
+}
+
+export default Report;
