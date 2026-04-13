2d937e22801d62c330e3267a365340987338469d
XD133713 <xd4133713@gmail.com>
2026-04-13 13:22:15 +0200

    Update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import SavedCalculator, UserCounter
+from .models import SavedCalculator, UserCounter, CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'is_admin', 'is_active')
 
 @admin.register(SavedCalculator)
 class SavedCalculatorAdmin(admin.ModelAdmin):
@@ -10,6 +15,5 @@ class SavedCalculatorAdmin(admin.ModelAdmin):
 @admin.register(UserCounter)
 class UserCounterAdmin(admin.ModelAdmin):
     list_display = ('user', 'login_amount', 'calculator_amount')
-
     def calculator_amount(self, obj):
         return obj.user.savedcalculator_set.count()
